b5b9167ff523412c26b4625788d20fc342f4f0ee
mga44 <39274754+mga44@users.noreply.github.com>
2025-04-18 22:38:53 +0200

    Some more IT test for DB

--- src/test/java/com/mga44/complaint_service/complaint/integration/MongoDbIT.java
+++ src/test/java/com/mga44/complaint_service/complaint/integration/MongoDbIT.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 
+import java.time.LocalDate;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataMongoTest
@@ -31,5 +33,47 @@ class MongoDbIT {
         repository.deleteById(saved.getId());
 
         assertThat(repository.findById(saved.getId())).isEmpty();
+    }
+
+    @Test
+    void shouldStoreComplaintEntity() {
+        var complaint = ComplaintEntity.builder()
+                .productId(1L)
+                .complaintContent("some message")
+                .creationDate(LocalDate.ofEpochDay(100))
+                .userId(2L)
+                .country("PL")
+                .complaintCounter(1)
+                .build();
+
+        var saved = repository.save(complaint);
+        var fromDb = repository.findById(saved.getId()).orElseThrow();
+
+        assertThat(saved)
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(fromDb);
+    }
+
+    @Test
+    void shouldEnsureUniqueness() {
+        var complaint = ComplaintEntity.builder()
+                .productId(1L)
+                .userId(2L)
+                .build();
+
+        var complaint2 = ComplaintEntity.builder()
+                .productId(1L)
+                .userId(2L)
+                .build();
+
+        var saved = repository.save(complaint);
+        var saved2 = repository.save(complaint2);
+        var fromDb = repository.findById(saved2.getId()).orElseThrow();
+
+        assertThat(saved2)
+                .usingRecursiveComparison()
+                .ignoringFields("id")
+                .isEqualTo(fromDb);
     }
 }
