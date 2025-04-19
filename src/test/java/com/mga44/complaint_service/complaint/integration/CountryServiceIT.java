d15a0221318aeee67483fa3c96781101b113a6a9
mga44 <39274754+mga44@users.noreply.github.com>
2025-04-19 11:09:41 +0200

    Move IT test to integration package

--- src/test/java/com/mga44/complaint_service/complaint/integration/CountryServiceIT.java
+++ src/test/java/com/mga44/complaint_service/complaint/integration/CountryServiceIT.java
@@ -1,5 +1,6 @@
-package com.mga44.complaint_service.complaint.service;
+package com.mga44.complaint_service.complaint.integration;
 
+import com.mga44.complaint_service.complaint.service.CountryService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
